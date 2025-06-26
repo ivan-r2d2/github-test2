d86beebe7d660a78724f655c8417b13e86f4fa6b
ivan-r2d2 <ivan.calle.flores@gmail.com>
2025-06-26 17:33:47 -0500

    modificando dos archivos

--- BASICO/demo01.py
+++ BASICO/demo01.py
@@ -1,12 +1,3 @@
 # Print a classic greeting
-print("Hello, World!")
+print("Hello, Ivan!")
 
-# Assign values to variables
-num1 = 10
-num2 = 5
-
-# Perform an arithmetic operation
-sum_of_numbers = num1 + num2
-
-# Print the result using an f-string (formatted string literal)
-print(f"The sum of {num1} and {num2} is: {sum_of_numbers}")
--- demo.py
+++ demo.py
@@ -4,17 +4,5 @@
 print("Hello, World!")
 
 # Assign values to variables
-num1 = 10
-num2 = -5
-
-# Perform an arithmetic operation
-sum_of_numbers = num1 + num2
-
-# Print the result using an f-string (formatted string literal)
-print(f"The sum of {num1} and {num2} is: {sum_of_numbers}")
-
-# Demonstrate a conditional statement
-if sum_of_numbers > 10:
-    print("The sum is greater than 10.")
-else:
-    print("The sum is not greater than 10.")
+for i in range(5):
+  print("hi", i)
